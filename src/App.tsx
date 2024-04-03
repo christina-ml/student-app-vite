e3e6e3447f974023ca554dafd34af18094c2a262
Christina Loiacono <loiaconochristina@gmail.com>
2024-04-02 22:58:25 -0400

    Minor changes - SCSS for index.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import "./App.scss";
 import StudentList from "./components/studentList/StudentList";
-import.meta.env.VITE_API_URL;
 
 function App() {
-
 	return ( 
         <div className="App">
             <StudentList />
