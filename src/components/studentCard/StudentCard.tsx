449b58185191fa6c5ee27e4766c69f74e69ac1e9
Christina Loiacono <loiaconochristina@gmail.com>
2024-04-02 22:53:54 -0400

    Add StudentCard student details. Add interface types for Student and StudentCard Comment out grades for now, since API does not have grades. Add SCSS styling

--- src/components/studentCard/StudentCard.tsx
+++ src/components/studentCard/StudentCard.tsx
@@ -1,10 +1,54 @@
 import React from "react";
 import "./StudentCard.scss";
 
-const StudentCard = () => {
+interface Student {
+	id: number;
+	firstname: string;
+	lastname: string;
+	company: string;
+	skill: string;
+	pic: string;
+	city: string;
+	email: string;
+}
+
+// each student has the Student interface
+interface StudentCard {
+	student: Student;
+}
+
+const StudentCard = (student: StudentCard) => {
+	const { firstname, lastname, company, skill, pic, city, email, grades } =
+		student.student;
+
+	// functions
+	// const calculateAverage = (grades: string[]) => {
+	// let sum = 0;
+
+	// grades.map(grade => {
+	//     sum += Number(grade);
+	// });
+
+	// return sum / grades.length;
+
+	// 	const sum = grades.reduce((sum, val) => sum + Number(val), 0);
+	// 	return sum / grades.length;
+	// };
+
 	return (
-        <div>StudentCard</div>
-    )
+		<div className="studentCard">
+			<div>
+				<img src={pic} alt={`${firstname} photo`} />
+			</div>
+			<div className="studentCard__name">
+				{`${firstname}  ${lastname}`}
+			</div>
+			<div>Email: {email}</div>
+			<div>Company: {company}</div>
+			<div>Skill: {skill}</div>
+			{/* <div>Average: {calculateAverage(grades)}%</div> */}
+		</div>
+	);
 };
 
 export default StudentCard;
