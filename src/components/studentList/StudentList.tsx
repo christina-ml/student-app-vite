25af703766d2cd269c5d8efabeee2d3f33d6e0dc
Christina Loiacono <loiaconochristina@gmail.com>
2024-04-08 09:43:03 -0400

    Add SearchBar functionality to StudentList.tsx, show view for no results. Add SCSS styling

--- src/components/studentList/StudentList.tsx
+++ src/components/studentList/StudentList.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import "./StudentList.scss";
+import SearchBar from "../searchBar/SearchBar";
 import StudentCard from "../studentCard/StudentCard";
+import "./StudentList.scss";
 
 // API URL
 const API = import.meta.env.VITE_API_URL;
@@ -19,6 +20,7 @@ interface Student {
 const StudentList = () => {
 	// hooks
 	const [students, setStudents] = useState<Student[]>([]);
+	const [searchTerm, setSearchTerm] = useState<string>("");
 
 	// functions
 	useEffect(() => {
@@ -33,12 +35,29 @@ const StudentList = () => {
 		// update our students hook with the new data
 	}, []); // empty array means run on mount
 
+	// when search term is updated, this component will rerender
+	// what to do on a re-render?
+	let filteredStudents: Student[] = students;
+
+	if (searchTerm) {
+		filteredStudents = students.filter((student: Student) => {
+			const fullName: string = `${student.firstname} ${student.lastname}`;
+			const fullNameToLowerCase: string = fullName.toLowerCase();
+			const searchTermToLowerCase: string = searchTerm.toLowerCase();
+			return fullNameToLowerCase.includes(searchTermToLowerCase);
+		});
+	}
+
 	// return or TSX
 	return (
 		<div className="studentList">
-			{students.map((student: Student, index: number) => {
+			<SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+			{filteredStudents.map((student: Student, index: number) => {
 				return <StudentCard key={index} student={student} />;
 			})}
+			{filteredStudents.length == 0 && (
+				<div className="studentList__noResults">No Results</div>
+			)}
 		</div>
 	);
 };
