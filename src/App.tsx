72d6c227b15a68b9d8553bb7606a81d968ee7662
Christina Loiacono <loiaconochristina@gmail.com>
2024-04-10 13:05:45 -0400

    Install react-router-dom. Add routes in App.tsx. Update SCSS styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.scss";
 import StudentList from "./components/studentList/StudentList";
+import StudentDetailsPage from "./pages/StudentDetailPage";
 
 function App() {
-	return ( 
-        <div className="App">
-            <StudentList />
-        </div>
-    )
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/students/:studentId" element={<StudentDetailsPage />} />
+					<Route path="/" element={<StudentList />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
