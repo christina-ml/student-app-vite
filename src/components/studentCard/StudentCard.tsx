af503f762564bba8e617fd362c4e07f4223da358
Christina Loiacono <loiaconochristina@gmail.com>
2024-04-10 13:09:42 -0400

    StudentCard.tsx - Update toggle, Add Link tag. Update SCSS styling.

--- src/components/studentCard/StudentCard.tsx
+++ src/components/studentCard/StudentCard.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./StudentCard.scss";
 import { FaPlus, FaMinus } from "react-icons/fa";
 
@@ -27,7 +28,14 @@ interface StudentCard {
 
 const StudentCard = ({ student }: StudentCard) => {
 	// props deconstructed
-	const { firstname, lastname, company, skill, pic, city, email, 
+	const {
+		firstname,
+		lastname,
+		company,
+		skill,
+		pic,
+		city,
+		email,
 		// grades
 	} = student;
 
@@ -45,51 +53,61 @@ const StudentCard = ({ student }: StudentCard) => {
 		return sum / grades.length;
 	};
 
+	const toggleGrades = (e: React.MouseEvent<SVGElement, MouseEvent>) => {
+		e.stopPropagation();
+		e.preventDefault();
+		setShowGrades(!showGrades);
+	};
+
 	return (
 		<div className="studentCard">
-			<div className="studentCard__profilePic">
-				<img src={pic} alt={`${firstname} photo`} />
-			</div>
-			<div className="studentCard__info">
-				<div className="studentCard__name">
-					{`${firstname}  ${lastname}`}
+			<Link to={`/students/${student.id}`} state={{ student: student }}>
+				<div className="studentCard__profilePic">
+					<img src={pic} alt={`${firstname} photo`} />
 				</div>
-				<div className="studentCard__infoLine">Email: {email}</div>
-				<div className="studentCard__infoLine">Company: {company}</div>
-				<div className="studentCard__infoLine">Skill: {skill}</div>
-				<div className="studentCard__infoLine">
-					Average: {calculateAverage(grades)}%
+				<div className="studentCard__info">
+					<div className="studentCard__name">
+						{`${firstname}  ${lastname}`}
+					</div>
+					<div className="studentCard__infoLine">Email: {email}</div>
+					<div className="studentCard__infoLine">
+						Company: {company}
+					</div>
+					<div className="studentCard__infoLine">Skill: {skill}</div>
+					<div className="studentCard__infoLine">
+						Average: {calculateAverage(grades)}%
+					</div>
+					<div
+						className="studentCard__gradesList"
+						style={{ display: showGrades ? "block" : "none" }}
+					>
+						{grades.map((grade: string, index: number) => {
+							return (
+								<div key={index}>
+									<span>Test {index + 1}:</span>
+									<span>{grade}%</span>
+								</div>
+							);
+						})}
+					</div>
 				</div>
-				<div
-					className="studentCard__gradesList"
-					style={{ display: showGrades ? "block" : "none" }}
-				>
-					{grades.map((grade: string, index: number) => {
-						return (
-							<div key={index}>
-								<span>Test {index + 1}:</span>
-								<span>{grade}%</span>
-							</div>
-						);
-					})}
+				<div className="studentCard__toggleIcons">
+					{!showGrades && (
+						<FaPlus
+							className="studentCard__toggleIcon"
+							onClick={(e) => toggleGrades(e)}
+							size="1.8em"
+						/>
+					)}
+					{showGrades && (
+						<FaMinus
+							className="studentCard__toggleIcon"
+							onClick={(e) => toggleGrades(e)}
+							size="1.8em"
+						/>
+					)}
 				</div>
-			</div>
-			<div className="studentCard__toggleIcons">
-				{!showGrades && (
-					<FaPlus
-						className="studentCard__toggleIcon"
-						onClick={() => setShowGrades(true)}
-						size="1.8em"
-					/>
-				)}
-				{showGrades && (
-					<FaMinus
-						className="studentCard__toggleIcon"
-						onClick={() => setShowGrades(false)}
-						size="1.8em"
-					/>
-				)}
-			</div>
+			</Link>
 		</div>
 	);
 };
