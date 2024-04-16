01fef4ed340feef9c9a8979755652a01c5515b24
Christina Loiacono <loiaconochristina@gmail.com>
2024-04-16 16:58:52 -0400

    Add Navbar Layout, SCSS styling. Add to App.tsx. Minor SCSS change to App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.scss";
+import Navbar from "./layout/navbar/Navbar";
 import StudentList from "./components/studentList/StudentList";
 import StudentDetailsPage from "./pages/StudentDetailPage";
 
@@ -9,8 +10,12 @@ function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
+				<Navbar />
 				<Routes>
-					<Route path="/students/:studentId" element={<StudentDetailsPage />} />
+					<Route
+						path="/students/:studentId"
+						element={<StudentDetailsPage />}
+					/>
 					<Route path="/" element={<StudentList />} />
 				</Routes>
 			</BrowserRouter>
